79d242c7cec0519a9db711090018768efec93d98
anisa adelya <anisa.adeliya@gmail.com>
2023-04-01 12:14:04 +0700

    menambah folder

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function HomePage() {
+        return view('homepage.index');
+    }
+
+    public function AboutPage() {
+        return view('homepage.about');
+    }
+
+    public function ContactPage() {
+        return view('homepage.contact');
+    }
+
+    public function ServicePage() {
+        return view('homepage.service');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/Coba', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/Home', function() {
+    return view('homepage.index');
+});
+
+Route::get('/About', [AboutController::class, 'AboutPage'])->name('about');
+
+Route::get('/Contact', [AboutController::class, 'ContactPage'])->name('contact');
+
+Route::get('/Service', [AboutController::class, 'ServicePage'])->name('service');
+
